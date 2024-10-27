6e9c055e9fbf58cd02ff03b0a4492efec517d929
MrJ0nik <142501317+MrJ0nik@users.noreply.github.com>
2024-10-28 00:01:44 +0200

    first commit

---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,31 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../lab 8.1it/lab 8.1it.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest1
+{
+	TEST_CLASS(UnitTest1)
+	{
+	public:
+
+		TEST_METHOD(TestFindThirdDot)
+		{
+			
+			char str[] = "a.b.c.d.e";  
+			Assert::AreEqual(5, find(str));  
+
+		}
+
+		TEST_METHOD(TestChangeEveryFourthCharacter)
+		{
+			
+			char str[] = "abcdefghi";  
+			char expected1[] = "abc.efg.i";
+			Assert::AreEqual(expected1, change(str)); 
+
+
+		}
+	};
+}
---
+++ lab 8.1it/lab 8.1it.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+using namespace std;
+
+int find(char s[]) {
+    int k = 0;
+    for (int i = 0; s[i] != 0; i++) {
+        if (s[i] == '.')
+            k++;
+        if (k == 3)
+            return i;
+    }
+    return -1;  
+}
+
+char* change(char* str) {
+    size_t len = strlen(str);
+
+    for (int i = 3; i < len; i += 4)  
+        str[i] = '.';
+    return str;
+}
+
+int main() {
+    char str[101];
+
+    cout << "Enter string:" << endl;
+    cin.getline(str, 100);
+
+    int third_dot_index = find(str);
+    if (third_dot_index != -1)
+        cout << "Index of third dot: " << third_dot_index << endl;
+    else
+        cout << "The string does not contain three dots." << endl;
+
+    char* modified_str = change(str);
+
+    cout << "Modified string (result): " << modified_str << endl;
+
+    return 0;
+}
